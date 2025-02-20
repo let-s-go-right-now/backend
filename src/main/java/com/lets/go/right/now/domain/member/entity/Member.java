021fc6bf74796ac0d24f77d181ef80d6c3e3adba
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-02-20 22:51:00 +0900

    🎨 [chore] Member 엔티티 작성

--- src/main/java/com/lets/go/right/now/domain/member/entity/Member.java
+++ src/main/java/com/lets/go/right/now/domain/member/entity/Member.java
@@ -10,10 +10,11 @@ import lombok.*;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "Member")
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String password;
     private String role; // 사용자 권한
+    private String accountNumber; // 계좌 번호
+    private String profileImgLink; // 프로필 이미지 링크 - S3
 }
