a7881f2eab988ccba4427314f21bf818d5b89fd3
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-02-21 00:31:09 +0900

    🎨 [add] 지출에 제외할 회원 엔티티 작성 및 연관관계 설정

---
+++ src/main/java/com/lets/go/right/now/domain/expense/entity/ExcludedMember.java
@@ -0,0 +1,35 @@
+package com.lets.go.right.now.domain.expense.entity;
+
+import com.lets.go.right.now.domain.member.entity.Member;
+import com.lets.go.right.now.domain.member.service.MemberService;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "Excluded_Member")
+public class ExcludedMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // == 연관 관계 설정 == //
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "excluded_member_id")
+    private Member excludedMember;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "expense_id")
+    private Expense expense;
+}
