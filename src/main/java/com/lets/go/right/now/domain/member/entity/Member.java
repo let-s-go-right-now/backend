19d98f866203566376ac1374ffdb8fc43ed876e3
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-02-20 22:51:59 +0900

    ✏️ [fix] Member 엔티티 속성명 수정

--- src/main/java/com/lets/go/right/now/domain/member/entity/Member.java
+++ src/main/java/com/lets/go/right/now/domain/member/entity/Member.java
@@ -11,10 +11,13 @@ import lombok.*;
 @Table(name = "Member")
 public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
     private String email;
     private String password;
     private String role; // 사용자 권한
+    @Column(name = "account_number")
     private String accountNumber; // 계좌 번호
+    @Column(name = "profile_image_link")
     private String profileImgLink; // 프로필 이미지 링크 - S3
 }
