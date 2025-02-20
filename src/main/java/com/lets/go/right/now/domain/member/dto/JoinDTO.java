799b9b2f3dee13a723b5dc738f48c478aea9c701
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-02-20 22:57:53 +0900

    🎨 [add] 회원가입 DTO 작성
    JoinDTO

--- src/main/java/com/lets/go/right/now/domain/member/dto/JoinDTO.java
+++ src/main/java/com/lets/go/right/now/domain/member/dto/JoinDTO.java
@@ -1,11 +1,23 @@
 package com.lets.go.right.now.domain.member.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * 회원 가입에 사용
+ */
 @Data
 @Builder
 public class JoinDTO {
+    @NotNull(message = "회원 이름은 필수입니다.")
+    private String name;
+    @Email(message = "이메일 형식을 지켜주세요")
+    @NotNull(message = "이메일은 필수입니다.")
     private String email;
+    @NotNull(message = "비밀번호는 필수입니다.")
     private String password;
+    @NotNull(message = "계좌 번호는 필수입니다.")
+    private String accountNumber; // 계좌 번호
 }
--- src/main/java/com/lets/go/right/now/domain/member/entity/Member.java
+++ src/main/java/com/lets/go/right/now/domain/member/entity/Member.java
@@ -13,6 +13,7 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
+    private String name; // 회원 이름
     private String email;
     private String password;
     private String role; // 사용자 권한
