af0977031759409d020c702b8ee9e4cbc3680758
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-02-22 19:08:32 +0900

    ✏️ [fix] 회원가입 로직에 이미지 저장 기능 추가

--- src/main/java/com/lets/go/right/now/domain/member/controller/MemberController.java
+++ src/main/java/com/lets/go/right/now/domain/member/controller/MemberController.java
@@ -4,12 +4,16 @@ import com.lets.go.right.now.domain.member.dto.JoinReq;
 import com.lets.go.right.now.domain.member.dto.LoinReq;
 import com.lets.go.right.now.domain.member.service.MemberService;
 import jakarta.validation.Valid;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/member")
@@ -29,7 +33,9 @@ public class MemberController {
      * 회원 가입
      */
     @PostMapping("/join")
-    public ResponseEntity<?> join(@Valid @RequestBody JoinReq joinReq) {
-        return memberService.join(joinReq);
+    public ResponseEntity<?> join(
+            @RequestParam(value = "image", required = false) MultipartFile image,
+            @Valid @ModelAttribute JoinReq joinReq) throws IOException {
+        return memberService.join(joinReq, image);
     }
 }
--- src/main/java/com/lets/go/right/now/domain/member/service/MemberService.java
+++ src/main/java/com/lets/go/right/now/domain/member/service/MemberService.java
@@ -2,9 +2,11 @@ package com.lets.go.right.now.domain.member.service;
 
 import com.lets.go.right.now.domain.member.dto.JoinReq;
 import com.lets.go.right.now.domain.member.dto.LoinReq;
+import java.io.IOException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface MemberService {
     ResponseEntity<?> login(LoinReq dto);
-    ResponseEntity<?> join(JoinReq joinReq);
+    ResponseEntity<?> join(JoinReq joinReq, MultipartFile image) throws IOException;
 }
