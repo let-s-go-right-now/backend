b6b3def26d33b00ce9fe5f29db88bb6c5be488f5
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-02-26 18:12:14 +0900

    🎨 [add] 송금 현황 기록 API 기본 구조 작성

--- src/main/java/com/lets/go/right/now/domain/settlement/dto/PaymentCreateReq.java
+++ src/main/java/com/lets/go/right/now/domain/settlement/dto/PaymentCreateReq.java
@@ -4,7 +4,7 @@ package com.lets.go.right.now.domain.settlement.dto;
  * 미리 정산한 금액 기록용
  * 누구에게 얼마를 보냈는지
  */
-public record PrepaymentCreateReq(
+public record PaymentCreateReq(
         String receiverEmail,
         Integer amount
 ) {
