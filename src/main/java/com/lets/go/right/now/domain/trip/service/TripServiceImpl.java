7812c60d50c9e392bdad1b0aa9d0b6ec2314bb0c
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-03-09 17:36:43 +0900

    ✏️ [fix] 여행 생성시 생성한 회원을 여행 회원으로 등록

--- src/main/java/com/lets/go/right/now/domain/trip/service/TripServiceImpl.java
+++ src/main/java/com/lets/go/right/now/domain/trip/service/TripServiceImpl.java
@@ -4,6 +4,7 @@ import com.lets.go.right.now.domain.member.entity.Member;
 import com.lets.go.right.now.domain.trip.dto.TripDetailResponse;
 import com.lets.go.right.now.domain.trip.dto.TripListDto;
 import com.lets.go.right.now.domain.trip.entity.Trip;
+import com.lets.go.right.now.domain.trip.entity.TripMember;
 import com.lets.go.right.now.domain.trip.repository.TripRepository;
 import com.lets.go.right.now.domain.tripMember.repository.TripMemberRepository;
 import com.lets.go.right.now.global.enums.statuscode.ErrorStatus;
@@ -32,6 +33,8 @@ public class TripServiceImpl implements TripService {
                 .endDate(endDate)
                 .owner(owner)
                 .build();
+        TripMember tripMember = TripMember.toEntity(trip, owner);
+        tripMemberRepository.save(tripMember);
         return tripRepository.save(trip);
     }
 
