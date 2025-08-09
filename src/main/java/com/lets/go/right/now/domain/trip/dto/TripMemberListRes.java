95d9d21b71c75a117f7f3f6cbca21b566e80e55d
oculo0204 <oculo0204@gmail.com>
2025-08-09 22:53:24 +0900

    !hotfix: member id

--- src/main/java/com/lets/go/right/now/domain/trip/dto/TripMemberListRes.java
+++ src/main/java/com/lets/go/right/now/domain/trip/dto/TripMemberListRes.java
@@ -7,9 +7,13 @@ import java.util.List;
 public record TripMemberListRes(List<MemberResDto> members) {
 
     // 각 멤버의 정보를 담는 DTO
-    public record MemberResDto(String name, String profileImageLink) {
+    public record MemberResDto(Long id, String name, String profileImageLink) {
         public MemberResDto(Member member) {
-            this(member.getName(), member.getProfileImgLink());
+            this(
+                    member.getId(),              // 고유 식별자 추가
+                    member.getName(),
+                    member.getProfileImgLink()
+            );
         }
     }
 }
