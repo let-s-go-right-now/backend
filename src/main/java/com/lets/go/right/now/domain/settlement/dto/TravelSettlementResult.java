8a9dd4088c0dcb56feb565032e51469239027cff
chaewonkim <tw2ysbe@naver.com>
2025-08-10 11:20:16 +0900

    :pencil2: [fix] 여행 정산 결과 확인 API 반환값 추가

--- src/main/java/com/lets/go/right/now/domain/settlement/dto/TravelSettlementResult.java
+++ src/main/java/com/lets/go/right/now/domain/settlement/dto/TravelSettlementResult.java
@@ -1,7 +1,6 @@
 package com.lets.go.right.now.domain.settlement.dto;
 
 import com.lets.go.right.now.domain.expense.dto.MemberProfileViewRes;
-import com.lets.go.right.now.domain.member.entity.Member;
 import com.lets.go.right.now.domain.settlement.entity.TravelSettlement;
 import com.lets.go.right.now.global.enums.Status;
 
@@ -9,6 +8,7 @@ import com.lets.go.right.now.global.enums.Status;
  * 여행 정산 결과에 대한 정보
  */
 public record TravelSettlementResult(
+        Long id, // trip_settlement_id
         MemberProfileViewRes sender, // 돈을 보낼 사람
         MemberProfileViewRes receiver, // 돈을 받을 사람
         Integer amount, // 보내야 할 돈
@@ -18,6 +18,11 @@ public record TravelSettlementResult(
         MemberProfileViewRes sender = MemberProfileViewRes.of(travelSettlement.getSender());
         MemberProfileViewRes receiver = MemberProfileViewRes.of(travelSettlement.getReceiver());
         return new TravelSettlementResult(
-                sender,receiver, travelSettlement.getAmount(), travelSettlement.getSettlementStatus());
+                travelSettlement.getId(),
+                sender,
+                receiver,
+                travelSettlement.getAmount(),
+                travelSettlement.getSettlementStatus()
+        );
     }
 }
